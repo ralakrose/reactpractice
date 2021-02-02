493843a25c23f8fc25bd264e48f9682dec094de7
Rose Ralak <ralakrose@gmail.com>
2021-02-02 09:37:18 +0300

    Input component and Character counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Input from "./components/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    paragraph: "",
+  };
+
+  characterLength = (event) => {
+    this.setState({
+      paragraph: event.target.value.length,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Input changed={this.characterLength} length={this.state.paragraph} />
+      </div>
+    );
+  }
 }
 
 export default App;
